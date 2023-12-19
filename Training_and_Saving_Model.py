d52929ae38b7589e19ab39e08a73f81276b319db
abhijith <abhijithsj4@gmail.com>
2023-12-19 23:32:25 +0530

    added save and loadNetwork functions

--- Training_and_Saving_Model.py
+++ Training_and_Saving_Model.py
@@ -18,14 +18,14 @@ print(Y)
 
 # network creation and training
 
-inputl = Layer(neuronCount=2,position=1)
+inputLayer = Layer(neuronCount=2,position=1)
 hidden = Layer(neuronCount=10,position=2,activation="linear")
 hidden2 = Layer(neuronCount=4,position=3,activation="linear")
 output = Layer(neuronCount=1,position=-1,activation="linear")
 
 network = Network(X=X,Y=Y,learningRate=0.0002,epoch=1000,errorThresh=3)
 
-network.add(layer=inputl)
+network.add(layer=inputLayer)
 network.add(layer=hidden)
 network.add(layer=hidden2)
 network.add(layer=output)
